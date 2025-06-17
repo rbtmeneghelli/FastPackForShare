264835c2e6b262894dd1e27782b3590d0b0580de
Roberto Meneghelli <r.grigoragi@avivatec.com.br>
2025-06-17 14:19:32 -0300

    Versão 2.9 - Add Base para validar se dois objetos do mesmo tipo são iguais
    Commit (Versão 2.9 - Add Base para validar se dois objetos do mesmo tipo são iguais)

---
+++ FastPackForShare/FastPackForShare/Bases/BaseEntityComparer.cs
@@ -0,0 +1,16 @@
+﻿using FastPackForShare.Default;
+
+namespace FastPackForShare.Bases;
+
+public class BaseEntityComparer<TBaseEntityModel> : IEqualityComparer<TBaseEntityModel> where TBaseEntityModel : BaseEntityModel
+{
+    public bool Equals(TBaseEntityModel x, TBaseEntityModel y)
+    {
+        return x.Id.Equals(y.Id);
+    }
+
+    public int GetHashCode(TBaseEntityModel obj)
+    {
+        return obj.Id.GetHashCode();
+    }
+}
