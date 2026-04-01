70bef731c77f98ad1a5b105af1724a9f10b39022
Roberto Meneghelli <roberto.mng.89@gmail.com>
2026-03-31 22:39:50 -0300

    Versão 5.9 - Ajustes pontuais e add nova criptografia
    Commit (Versão 5.9 - Ajustes pontuais e add nova criptografia)

---
+++ FastPackForShare/FastPackForShare/Cryptographys/CryptographyBcryptManager.cs
@@ -0,0 +1,8 @@
+﻿namespace FastPackForShare.Cryptographys;
+
+public static class CryptographyBcryptManager
+{
+    public static string ApplyHashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+
+    public static bool ValidateHash(string password, string hash) => BCrypt.Net.BCrypt.Verify(password, hash);
+}
